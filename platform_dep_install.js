cb0b48f391c000eca7518bbcea8bf5f15dffd692
Maurits Lamers <maurits@weidestraat.nl>
2014-04-30 21:52:08 +0200

    make it work

--- platform_dep_install.js
+++ platform_dep_install.js
@@ -1,6 +1,12 @@
 // is a real
 var os = require('os');
-var cp = require('child-process');
+var cp = require('child_process');
 if (os.platform() === "darwin") {
-  cp.spawn("npm install git://github.com/mauritslamers/fsevents-bin");
+  var proc = cp.spawn("npm", ["install", "git://github.com/mauritslamers/fsevents-bin"]);
+  proc.stdout.on('data', function (d) {
+    console.log(d.toString());
+  });
+  proc.stderr.on('data', function (d) {
+    console.log(d.toString());
+  });
 }
