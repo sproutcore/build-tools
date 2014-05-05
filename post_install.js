c4e1717537942476b0ac733b521bfdfc20146948
Maurits Lamers <maurits@weidestraat.nl>
2014-05-05 23:55:58 +0200

    trying to hook the doneInstalling function to the BT env

--- post_install.js
+++ post_install.js
@@ -16,9 +16,11 @@ util.log('Installing sproutcore as global dependency...');
 var code = "SC.run( function() { \n";
 code += "  BT.startInstall('git://github.com/sproutcore/sproutcore', true, {\n";
 code += "    silent: true, frameworkName: 'sproutcore', branch: 'team/mauritslamers/newbt'}); \n";
-code += "  });";
+code += "  });\n";
+
 buildtools.__env.setPath('BT.runMode', "install");
 buildtools.__env.setPath('BT.btPath', __dirname);
+buildtools.__env.setPath('BT._installDidFinish', doneInstalling);
 buildtools.__env.runCode(code);
 
 // util.log("SproutCore has been installed.");
