sproutcore/build-tools

commit 4f060ddc09b3b2722d3e0ed8d2a4100cb583fbc3

Makes sure findResourceFor return files matching the file name
Previously, all the files containing the file name were return, which
mean my_file.woff2 were also returned when looking for my_file.woff

lib/framework.js
@@ -759,7 +759,7 @@ BT.Framework = SC.Object.extend({
 
     var ret = resources.filter(function (f) {
       var p = f.get('path');
-      if (p.indexOf(filename) > -1) return true;
+      if (p.indexOf(filename, p.length - filename.length) !== -1) return true;
     });
     return ret;
   },

commit 249f6e24f394fe8bc10f3602c89c7a86243f87da

fixing an oversight introduced in commit 4f060dd, because not all files are referred to by full file name. This adds a fail safe where if the file cannot be found directly, it is searched for indirectly.

lib/framework.js
@@ -757,10 +757,20 @@ BT.Framework = SC.Object.extend({
     var firstTry = resources.findProperty('path', filepath);
     if (firstTry) return [firstTry];
 
+    // first try a full match on the file name
     var ret = resources.filter(function (f) {
       var p = f.get('path');
       if (p.indexOf(filename, p.length - filename.length) !== -1) return true;
     });
+
+    // if still not found, try a partial match. This is useful because often resources are required without
+    // file extension.
+    if (ret.length === 0) {
+      ret = resources.filter(function (f) {
+        var p = f.get('path');
+        if (p.indexOf(filename) !== -1) return true;
+      });
+    }
     return ret;
   },
 