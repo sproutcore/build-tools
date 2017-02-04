e0915f075bf7789d46d7bf4c2bedca38c76fecf3
Maurits Lamers <maurits@weidestraat.nl>
2017-02-04 12:07:47 +0100

    Working project init in a given directory

--- generators/project/generator.js
+++ generators/project/generator.js
@@ -22,18 +22,28 @@ function mkDir(path) {
 commander
   .version('0.0.1')
   .arguments("<folder> <name>")
-  .action(function (folder, name) {
+  .option("--skip-create-projectfolder", "Skip the step of creating a project folder and use the folder given")
+  .action(function (folder, name, opts) {
     if (! (folder && name) ) { // this check is probably not neccesary, as commander already does the checking IIRC
       console.log("The project generator needs both a folder and a name");
       return;
     }
-    var projFolder = path.join(folder, name);
+    var projFolder;
+    if (!opts.skipCreateProjectfolder) {
+      projFolder = path.join(folder, name);
+      mkDir(path.join(projFolder, "apps"));
+    }
+    else projFolder = folder;
+    
+    // create a few basic folders
+    ['apps','frameworks'].forEach(function (f) {
+      mkDir(path.join(projFolder, f));
+    });
+
     var config = fs.readFileSync(path.join(generatorDir, "sc_config.ejs")).toString();
     // console.log('projFolder: ' + projFolder);
     // console.log('config: ' + config);
 
-    mkDir(projFolder);
-    mkDir(path.join(projFolder, "apps"));
     try {
       // we're not using the ejs options here (yet), so no ejs parsing required
       fs.writeFileSync(path.join(projFolder, "sc_config"), config);
